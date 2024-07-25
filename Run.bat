6e410b672956bc21e22c394f9b78fc03df20480b
ael2222222222222222222222222222 <130606121+ael2222222222222222222222222222@users.noreply.github.com>
2024-07-25 05:46:38 -0700

    Add files via upload

---
+++ Run.bat
@@ -0,0 +1,3 @@
+PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0UnhideCPUPowerSettings.ps1""' -Verb RunAs}"
+PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0DisablePNP-Powersaving.ps1""' -Verb RunAs}"
+PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0MsiMode.ps1""' -Verb RunAs}"
