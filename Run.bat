568def9746efd32f345900440bfecb183bb25aa4
ael2222222222222222222222222222 <130606121+ael2222222222222222222222222222@users.noreply.github.com>
2024-07-25 05:44:36 -0700

    Delete Run.bat

--- Run.bat
+++
@@ -1,2 +0,0 @@
-PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0UnhideCPUPowerSettings.ps1""' -Verb RunAs}"
-PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0DisablePNP-Powersaving.ps1""' -Verb RunAs}"
