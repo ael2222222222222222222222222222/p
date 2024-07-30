5c1abac8d14140155373aa8aac25d303f130a724
ael2222222222222222222222222222 <130606121+ael2222222222222222222222222222@users.noreply.github.com>
2024-07-30 11:59:35 -0700

    Delete Ru1n.bat

--- Ru1n.bat
+++
@@ -1,2 +0,0 @@
-PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0StartMenu.ps1""' -Verb RunAs}"
-PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0Edge.ps1""' -Verb RunAs}"
